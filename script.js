bf0f44f4ed25bbc59cc442f0c2a4803365052fc8
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-15 19:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //complete this code
-class Animal {}
+class Animal {
+	constuctor(species){
+		 this.species=species;
+	}
+	makeSound(){
+		console.log(this.species + " makes a sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark()
+	{
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
